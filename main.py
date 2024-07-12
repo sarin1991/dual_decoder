7efb853ac81983032f7a6905c3b0ed2a977bf36a
Sarin Eapen Chandy <sarin1991@gmail.com>
2024-07-12 16:11:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import torch
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+from datasets import load_dataset
+from transformers import AutoTokenizer, TrainingArguments, AutoModelForCausalLM, AutoConfig
+from dataclasses import dataclass, field
+from typing import Optional
+torch.backends.cuda.matmul.allow_tf32=True
+
+@dataclass
+class CustomTrainingArguments(TrainingArguments):
+    training_dataset: str = field(default='JeanKaddour/minipile')
+    pretrained_model: str = field(default=None)
+    model_output_path: str = field(default=None)
+    max_seq_length: int = field(default=8192)
+    response_template: str = field(default="[/INST]")
+
+def main():
+    parser = transformers.HfArgumentParser(
+        (CustomTrainingArguments)
+    )
+    (training_args,) = parser.parse_args_into_dataclasses()
+    training_args.gradient_checkpointing_kwargs={"use_reentrant": False}
+
+    tokenizer = AutoTokenizer.from_pretrained(training_args.pretrained_model, model_max_length=training_args.max_seq_length,padding_side="right")
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.truncation_side = "left"
+
+    train_dataset = load_dataset('parquet', split="train", data_files=training_args.training_dataset)
+    model = AutoModelForCausalLM.from_pretrained(training_args.pretrained_model,attn_implementation="flash_attention_2",torch_dtype=torch.bfloat16)
+    model.to('cuda')
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        max_seq_length=training_args.max_seq_length,
+        tokenizer=tokenizer,
+    )
+    trainer.train()
+    trainer.save_model(training_args.model_output_path)
+
+if __name__=="__main__":
+    main()
